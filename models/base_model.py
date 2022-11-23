addc3d6c8733af5f79fd8d73f2085536cd3d5861
Bradley Kibs <Bradleykibs@gmail.com>
2022-11-23 13:42:58 +0300

    finishing the base model class

--- models/base_model.py
+++ models/base_model.py
@@ -2,22 +2,25 @@
 """
 class BaseModel defines all common attributes/methods for other classes
 """
-import datetime
-import uuid
-import time
+from datetime import datetime
+from uuid import uuid4
 
 class BaseModel:
     def __init__(self, id=None, created_at=None, updated_at=None):
-        self.id = uuid.uuid4().hex
-        self.created_at = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.id = uuid4().hex
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
     
     def __str__(self):
         class_name = self.__class__.__name__
         return f"[<{class_name}>] (<{self.id}>) <{self.__dict__}>"
 
     def save(self):
-         self.updated_at = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S    .%f")
+         self.updated_at = datetime.now()
 
     def to_dict(self):
-        return self.__all__.__dict__.items()
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
