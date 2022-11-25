728eefa2c076b174e1260152b3e8538f28930ed9
Domengo <dominicsengo@gmail.com>
2022-11-25 16:45:58 +0300

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 A file storing model that saves object instances as a string in a file and converts 
-It into Json when used"
+iIt into Json when used"
 """
 
 
@@ -14,7 +14,7 @@ class FileStorage:
     deserializes JSON file to instances
     """
 
-    __file_path = "file.json" 
+    __file_path = "./storage/file.json" 
     __objects = {}
 
     def all(self):
@@ -22,6 +22,7 @@ class FileStorage:
         public instance method that returns the
         dictionary __objects.
         """
+        return FileStorage.__objects
 
     def new(self, obj):
         """
@@ -33,7 +34,16 @@ class FileStorage:
         """
         if obj:
             key = f"{obj.__class__.__name__}.{obj.id}"
-            Filestorage.__objects[key] = obj
+            FileStorage.__objects[key] = obj
 
     def save(self):
+        """serializes __objects to the 
+        JSON file (path: __file_path)
+        """
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            new_dict[key] = value.to_dict().copy()
+        with open(FileStorage.__file_path, 'w') as my_file:
+            json.dump(new_dict, my_file)
+
 
